eb4cd1bd1c53da761c269485d81e99bf197d95f8
Sharktheone <sharktheone@proton.me>
2023-03-20 17:31:37 +0100

    flag string for config path

--- conf/conf.go
+++ conf/conf.go
@@ -7,37 +7,41 @@ import (
 	"os"
 )
 
-const confPath = "config/config5.yml"
+var (
+	confPath = flag.String("config", "config/config.yml", "Path to the config file")
+	Config   Format
+)
 
+func init() {
+	GetConf()
+}
 func GetConf() Format {
-	var config Format
 	ymlConf, err := os.ReadFile(*confPath)
 	if err != nil {
 		log.Fatalf("Failed to get config: %v", err)
 	}
-	if err := yaml.Unmarshal(ymlConf, &config); err != nil {
+	if err := yaml.Unmarshal(ymlConf, &Config); err != nil {
 		log.Fatalf("Failed to load config: %v", err)
 	}
-	return config
+	return Config
 }
-
 func GetServerConf(serverID string, serverName string) (serverConf Server) {
 	if serverID != "" && serverName != "" {
 		for _, server := range GetConf().Pterodactyl.Servers {
 			if server.ServerID == serverID && server.ServerName == serverName {
-				return Server(server)
+				return server
 			}
 		}
 	} else if serverID != "" {
 		for _, server := range GetConf().Pterodactyl.Servers {
 			if server.ServerID == serverID {
-				return Server(server)
+				return server
 			}
 		}
 	} else if serverName != "" {
 		for _, server := range GetConf().Pterodactyl.Servers {
 			if server.ServerName == serverName {
-				return Server(server)
+				return server
 			}
 		}
 	} else {
