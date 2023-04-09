25689cae366d0f79b3473c21ebea23bbdd8e9290
Sharktheone <sharktheone@proton.me>
2023-04-09 23:43:50 +0200

    fix existing event management to new event handling

--- srv/playersrv/events.go
+++ srv/playersrv/events.go
@@ -2,17 +2,26 @@ package playersrv
 
 import (
 	"Scharsch-Bot/discord/embed/srvEmbed"
-	"Scharsch-Bot/srv/api/handlers/websocket"
+	"Scharsch-Bot/types"
 	"fmt"
 	"log"
 	"strings"
 )
 
 func (p *PlayerSrv) SwitchEvents() {
-	switch p.eventJson.Type {
+	switch p.event.Event {
 	case "chat":
 		if p.server.Config.Chat.Embed {
-			messageEmbed := srvEmbed.Chat(*p.eventJson, *p.server.Config, p.footerIcon, p.username, s)
+
+			// Temporary conversion to types.EventJSON
+			e := types.EventJson{
+				Name:   p.event.Data.Player,
+				Value:  p.event.Data.DeathMessage,
+				Type:   p.event.Event,
+				Server: p.server.Config.ServerID,
+			}
+
+			messageEmbed := srvEmbed.Chat(e, *p.server.Config, p.footerIcon, p.username, s)
 			for _, channelID := range p.server.Config.Chat.ChannelID {
 				_, err := s.ChannelMessageSendEmbed(channelID, &messageEmbed)
 				if err != nil {
@@ -21,7 +30,7 @@ func (p *PlayerSrv) SwitchEvents() {
 			}
 		} else {
 			for _, channelID := range p.server.Config.Chat.ChannelID {
-				_, err := s.ChannelMessageSend(channelID, fmt.Sprintf("%v%v %v", p.eventJson.Name, p.server.Config.Chat.Prefix, p.eventJson.Value))
+				_, err := s.ChannelMessageSend(channelID, fmt.Sprintf("%v%v %v", p.event.Data.Player, p.server.Config.Chat.Prefix, p.event.Data.Message))
 				if err != nil {
 					log.Printf("Failed to send Chat (embed): %v (channelID: %v)", err, channelID)
 				}
@@ -30,7 +39,14 @@ func (p *PlayerSrv) SwitchEvents() {
 
 	case "death":
 		if p.server.Config.SRV.Events.Death {
-			messageEmbed := srvEmbed.PlayerDeath(*p.eventJson, *p.server.Config, p.footerIcon, p.username, s)
+			// Temporary conversion to types.EventJson
+			e := types.EventJson{
+				Name:   p.event.Data.Player,
+				Value:  p.event.Data.DeathMessage,
+				Type:   p.event.Event,
+				Server: p.server.Config.ServerID,
+			}
+			messageEmbed := srvEmbed.PlayerDeath(e, *p.server.Config, p.footerIcon, p.username, s)
 			for _, channelID := range p.server.Config.SRV.ChannelID {
 				_, err := s.ChannelMessageSendEmbed(channelID, &messageEmbed)
 				if err != nil {
@@ -40,12 +56,7 @@ func (p *PlayerSrv) SwitchEvents() {
 		}
 	case "advancement":
 		if p.server.Config.SRV.Events.Advancement {
-			// Temporary conversion to websocket.Event
-			e := websocket.Event{}
-			e.Data.Player = p.eventJson.Name
-			e.Data.Advancement = p.eventJson.Value
-
-			messageEmbed := srvEmbed.PlayerAdvancement(&e, p.server.Config, &p.footerIcon, &p.username, s)
+			messageEmbed := srvEmbed.PlayerAdvancement(p.event, p.server.Config, &p.footerIcon, &p.username, s)
 			for _, channelID := range p.server.Config.SRV.ChannelID {
 				_, err := s.ChannelMessageSendEmbed(channelID, &messageEmbed)
 				if err != nil {
@@ -57,9 +68,9 @@ func (p *PlayerSrv) SwitchEvents() {
 		if p.server.Config.SRV.Events.PlayerJoinLeft {
 			p.server.OnlinePlayers.Mu.Lock()
 			defer p.server.OnlinePlayers.Mu.Unlock()
-			name := strings.ToLower(p.eventJson.Name)
+			name := strings.ToLower(p.event.Data.Player)
 			*p.server.OnlinePlayers.Players = append(*p.server.OnlinePlayers.Players, name)
-			messageEmbed := srvEmbed.PlayerJoin(*p.server.Config, strings.ToLower(p.eventJson.Name), p.footerIcon, p.username, s)
+			messageEmbed := srvEmbed.PlayerJoin(*p.server.Config, strings.ToLower(p.event.Data.Player), p.footerIcon, p.username, s)
 			for _, channelID := range p.server.Config.SRV.ChannelID {
 				_, err := s.ChannelMessageSendEmbed(channelID, &messageEmbed)
 				if err != nil {
@@ -72,13 +83,13 @@ func (p *PlayerSrv) SwitchEvents() {
 			p.server.OnlinePlayers.Mu.Lock()
 			defer p.server.OnlinePlayers.Mu.Unlock()
 			for i, player := range *p.server.OnlinePlayers.Players {
-				if player == strings.ToLower(p.eventJson.Name) {
+				if player == strings.ToLower(p.event.Data.Player) {
 					players := *p.server.OnlinePlayers.Players
 					*p.server.OnlinePlayers.Players = append(players[:i], players[i+1:]...)
 					break
 				}
 			}
-			messageEmbed := srvEmbed.PlayerQuit(*p.server.Config, strings.ToLower(p.eventJson.Name), p.footerIcon, p.username, s)
+			messageEmbed := srvEmbed.PlayerQuit(*p.server.Config, strings.ToLower(p.event.Data.Player), p.footerIcon, p.username, s)
 			for _, channelID := range p.server.Config.SRV.ChannelID {
 				_, err := s.ChannelMessageSendEmbed(channelID, &messageEmbed)
 				if err != nil {
