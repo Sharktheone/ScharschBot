1bb12ffc9f16f0b7e4bd23c57bc3d9958a133488
Sharktheone <sharktheone@proton.me>
2023-04-12 20:52:34 +0200

    remove unneeded args

--- srv/api/handlers/websocket/handle.go
+++ srv/api/handlers/websocket/handle.go
@@ -4,7 +4,6 @@ import (
 	"Scharsch-Bot/conf"
 	"Scharsch-Bot/discord/embed/srvEmbed"
 	"Scharsch-Bot/types"
-	"context"
 )
 
 var (
@@ -12,77 +11,75 @@ var (
 )
 
 func (p *PSRVEvent) processEvent() {
-	ctx := &p.h.ctx
-	e := p.e
-	if p.h.authenticated == false && e.Event != Auth {
+	if p.h.authenticated == false && p.e.Event != Auth {
 		return
 	}
-	switch e.Event {
+	switch p.e.Event {
 	case SendPlayers:
-		p.sendPlayers(ctx, e)
+		p.sendPlayers()
 	case KickPlayer:
-		p.kickPlayer(ctx, e)
+		p.kickPlayer()
 	case BanPlayer:
-		p.banPlayer(ctx, e)
+		p.banPlayer()
 	case UnbanPlayer:
-		p.unbanPlayer(ctx, e)
+		p.unbanPlayer()
 	case PlayerJoined:
-		p.playerJoined(ctx, e)
+		p.playerJoined()
 	case PlayerLeft:
-		p.playerLeft(ctx, e)
+		p.playerLeft()
 	case Players:
-		p.players(ctx, e)
+		p.players()
 	case ChatMessage:
-		p.chatMessage(ctx, e)
+		p.chatMessage()
 	case PlayerDeath:
-		p.playerDeath(ctx, e)
+		p.playerDeath()
 	case PlayerAdvancement:
-		p.playerAdvancement(ctx, e)
+		p.playerAdvancement()
 	case Auth:
-		p.auth(ctx, e)
+		p.auth()
 	}
 }
 
 // sendPlayers send total online players to server
-func (p *PSRVEvent) sendPlayers(ctx *context.Context, e *types.WebsocketEvent) {
+func (p *PSRVEvent) sendPlayers() {
 
 }
 
 // kickPlayer kick player on all servers
-func (p *PSRVEvent) kickPlayer(ctx *context.Context, e *types.WebsocketEvent) {
+func (p *PSRVEvent) kickPlayer() {
 
 }
 
 // banPlayer ban player on all servers
-func (p *PSRVEvent) banPlayer(ctx *context.Context, e *types.WebsocketEvent) {
+func (p *PSRVEvent) banPlayer() {
 
 }
 
-func (p *PSRVEvent) unbanPlayer(ctx *context.Context, e *types.WebsocketEvent) {
+func (p *PSRVEvent) unbanPlayer() {
 
 }
 
-func (p *PSRVEvent) playerJoined(ctx *context.Context, e *types.WebsocketEvent) {
+func (p *PSRVEvent) playerJoined() {
 	if p.h.server.Config.SRV.Events.PlayerJoinLeft {
-		*p.h.server.OnlinePlayers.Players = append(*p.h.server.OnlinePlayers.Players, e.Data.Player)
+		*p.h.server.OnlinePlayers.Players = append(*p.h.server.OnlinePlayers.Players, p.e.Data.Player)
 	}
 }
 
-func (p *PSRVEvent) playerLeft(ctx *context.Context, e *types.WebsocketEvent) {
+func (p *PSRVEvent) playerLeft() {
 	if p.h.server.Config.SRV.Events.PlayerJoinLeft {
-		*p.h.server.OnlinePlayers.Players = append(*p.h.server.OnlinePlayers.Players, e.Data.Player)
+		*p.h.server.OnlinePlayers.Players = append(*p.h.server.OnlinePlayers.Players, p.e.Data.Player)
 	}
 }
 
-func (p *PSRVEvent) players(ctx *context.Context, e *types.WebsocketEvent) {
+func (p *PSRVEvent) players() {
 	if p.h.server.Config.SRV.Events.PlayerJoinLeft {
 		p.h.server.OnlinePlayers.Mu.Lock()
 		defer p.h.server.OnlinePlayers.Mu.Unlock()
-		p.h.server.OnlinePlayers.Players = &e.Data.Players
+		p.h.server.OnlinePlayers.Players = &p.e.Data.Players
 	}
 }
 
-func (p *PSRVEvent) chatMessage(ctx *context.Context, e *types.WebsocketEvent) {
+func (p *PSRVEvent) chatMessage() {
 	if p.h.server.Config.Chat.Enabled {
 		if p.h.server.Config.Chat.Embed {
 			messageEmbed := srvEmbed.Chat(p.e, p.h.server.Config, p.footerIcon, p.username, p.session)
@@ -94,7 +91,7 @@ func (p *PSRVEvent) chatMessage(ctx *context.Context, e *types.WebsocketEvent) {
 	}
 }
 
-func (p *PSRVEvent) playerDeath(ctx *context.Context, e *types.WebsocketEvent) {
+func (p *PSRVEvent) playerDeath() {
 	if p.h.server.Config.SRV.Events.Death {
 		messageEmbed := srvEmbed.PlayerDeath(p.e, p.h.server.Config, p.footerIcon, p.username, p.session)
 		p.session.SendEmbeds(p.h.server.Config.SRV.ChannelID, messageEmbed, "Death")
@@ -102,15 +99,15 @@ func (p *PSRVEvent) playerDeath(ctx *context.Context, e *types.WebsocketEvent) {
 
 }
 
-func (p *PSRVEvent) playerAdvancement(ctx *context.Context, e *types.WebsocketEvent) {
+func (p *PSRVEvent) playerAdvancement() {
 	if p.h.server.Config.SRV.Events.Advancement {
 		messageEmbed := srvEmbed.PlayerAdvancement(p.e, p.h.server.Config, p.footerIcon, p.username, p.session)
 		p.session.SendEmbeds(p.h.server.Config.SRV.ChannelID, &messageEmbed, "Advancement")
 	}
 }
 
-func (p *PSRVEvent) auth(ctx *context.Context, e *types.WebsocketEvent) {
-	if e.Data.Password == config.SRV.API.Password && e.Data.User == config.SRV.API.User {
+func (p *PSRVEvent) auth() {
+	if p.e.Data.Password == config.SRV.API.Password && p.e.Data.User == config.SRV.API.User {
 		p.h.authenticated = true
 		p.h.send <- &types.WebsocketEvent{
 			Event: AuthSuccess,
